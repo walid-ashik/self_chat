823a85fd26115d9559619db1cb26098dc8ea1b53
walid-ashik <walid.al.ashik@gmail.com>
2024-01-05 12:28:36 +0600

    Initial Commit

---
+++ self_chat/self_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  self_chatApp.swift
+//  self_chat
+//
+//  Created by Walid Ashik on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct self_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
