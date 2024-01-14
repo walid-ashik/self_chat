5567a87162f6767c58423588b3b037d27291ba62
walid-ashik <walid.al.ashik@gmail.com>
2024-01-14 15:13:35 +0600

    make it floating

--- self_chat/ContentView.swift
+++ self_chat/ContentView.swift
@@ -6,21 +6,18 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//
+//struct ContentView: View {
+//    var body: some View {
+//        // Your main content here
+//        Text("Hello, World!")
+//            .padding()
+//    }
+//}
+//
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
--- self_chat/self_chatApp.swift
+++ self_chat/self_chatApp.swift
@@ -6,12 +6,58 @@
 //
 
 import SwiftUI
+import SwiftUI
 
 @main
-struct self_chatApp: App {
+struct SelfChatApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .windowStyle(.hiddenTitleBar)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .frame(width: 500, height: 500, alignment: .center)
+        .background(TransparentWindowBackground())
+    }
+}
+
+struct TransparentWindowBackground: NSViewRepresentable {
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    class Coordinator: NSObject {
+        
+    }
+    
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView()
+        DispatchQueue.main.async {
+            let window = view.window
+            window?.backgroundColor = NSColor.clear
+            window?.level = .floating
+            //            window?.styleMask.remove([.closable, .resizable])
+        }
+        return view
+    }
+    
+    func updateNSView(_ nsView: NSView, context: Context) {
+        
+    }
+    
+    static func dismantleNSView(_ nsView: NSView, coordinator: Coordinator) {
+        
     }
 }
